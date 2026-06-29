192efd89ec6bba42fda9d197421c17daab78aab2
Eric Farrar <erfarrar@gmail.com>
2026-06-28 21:50:01 -0400

    Add new tags

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "44753036517-h39giv3si9d9d8nagji9evpt4cior2sf.apps.googleusercontent.com";
 const SCOPES = "https://www.googleapis.com/auth/drive"
-const AVAILABLE_TAGS = ["legs", "core", "arms", "cardio", "stretch"];
+const AVAILABLE_TAGS = ["legs", "core", "arms", "cardio", "stretch", "ashley", "stephen", "bone fit", "all levels"];
 // Google Groups you can share videos with — replace with your real group addresses.
 const GROUPS = [
   { email: "inspiredtomove-bonefit@googlegroups.com", name: "Bone Fit" }
